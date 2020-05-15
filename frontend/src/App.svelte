2785fa5bb5cdf1ade483f3a3032021655f798ab0
victorjourne <journe.victor@gmail.com>
2020-05-15 13:10:23 +0200

    Add svelte app

---
+++ frontend/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import ResultItem from './ResultItem.svelte';
+	import ResultList from './ResultList.svelte';
+	import SearchBar from './SearchBar.svelte';
+
+</script>
+
+<SearchBar/>
+
+<ResultList>
+
+<ResultItem title={'Title A'} body={'Some text'} authors={'Author 1'}/>
+<ResultItem title={'Title B'} body={'Some text'} authors={'Author 2'}/>
+<ResultItem title={'Title C'} body={'Some text'} authors={'Author 3'}/>
+
+</ResultList>
---
+++ frontend/src/ResultItem.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let title;
+	export let body;
+	export let authors;
+</script>
+
+<section class='result-item'>
+
+<h2>{title}</h2>
+<p>{body}</p>
+<p>Auteurs : {authors}</p>
+
+</section>
+
+
+<style>
+	.result-item {
+		border: 1px solid #aaa;
+		border-radius: 2px;
+		box-shadow: 2px 2px 8px rgba(0,0,255,1);
+		padding: 1em;
+		margin: 0 0 1em 0;
+	}
+</style>
