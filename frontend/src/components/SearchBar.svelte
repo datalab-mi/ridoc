6392f33bff3358ab7ef61010f0abef7f1f0290bf
victor.journe <victor.journe@interieur.gouv.fr>
2020-07-08 16:56:09 +0200

    replace http://localhost/api by /api in frontend

--- frontend/src/components/SearchBar.svelte
+++ frontend/src/components/SearchBar.svelte
@@ -8,7 +8,7 @@
 
 	async function search() {
 
-		const res = await fetch("http://localhost/api/common/search",{
+		const res = await fetch("/api/common/search",{
 												method: "POST",
 												body: JSON.stringify({
 															 index_name: $index_name,
