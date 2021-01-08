c342c9c0a2304553666206b82c9a5a291bfc71e4
victorjourne <journe.victor@gmail.com>
2021-01-08 19:19:08 +0100

    WP, difficulties to pass css property to index html page

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -6,32 +6,50 @@
 </svelte:head>
 
 <div>
-{@html innerIndex}
+{@html indexHTML}
+</div>
+<h1>Guacamole rapide à faire</h1>
+<div>
+
+<h2>Ingrédients</h2>
+<ul>
+  <li>2 avocats (pelés et avec les noyaux retirés)</li>
+  <li>le jus d'un citron</li>
+  <li>¼ de concombre, coupé grossièrement</li>
+  <li>1 petite tomate, coupée</li>
+</ul>
+
+<h2>Instructions</h2>
+<ol>
+  <li>Écrasez délicatement les avocats avec une fourchette</li>
+  <li>Placez la purée obtenue dans un plat et arrosez avec le jus de citron</li>
+  <li>Mélangez pour que le jus de citron empêche la purée d'avocat de noircir</li>
+  <li>Mélangez la tomate et le concombre coupés</li>
+  <li>Gardez au frais et servir rapidement avec des tortillas</li>
+</ol>
 </div>
 
 <style>
-	h1 {
-	    font-size: 2rem;
-	}
-</style>
+	:global(h1) {font-size: 2rem;}
+	:global(ul) {list-style-type: circle; padding: revert;}
+	:global(a) {color: blue;}
 
+</style>
 
 <script context="module">
   export async function preload() {
     // the `slug` parameter is available because
     // this file is called [slug].html
-    const res = await this.fetch('innerIndex.html');
-    const innerIndex = await res.text();
+    const res = await this.fetch('indexHTML.html');
+    const indexHTML = await res.text();
 		console.log('preload')
-		console.log(innerIndex)
     if (res.status === 200) {
-      return { innerIndex };
+      return { indexHTML };
     } else {
-      this.error(res.status, data.message);
+      this.error(res.status, indexHTML.message);
     }
   }
 </script>
 <script>
-  export let innerIndex;
-	console.log(innerIndex)
+  export let indexHTML;
 </script>
