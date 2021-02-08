219e07dee23fb221073e9400984c69d2c406aedc
yerb-wavestone <yves.erb@wavestone.fr>
2021-02-08 17:24:01 +0100

    test avant affichage du menu mobile

--- frontend/src/layouts/Nav.svelte
+++ frontend/src/layouts/Nav.svelte
@@ -65,31 +65,33 @@
 					<div class="hidden sm:flex ml-2 font-bold text-2xl">Recherche dans les rapports IGA</div>
 					<div class="flex items-center justify-between h-10">
 						<div class="flex items-center">
-							<div class="hidden md:block">
-								<div class="ml-2 flex items-baseline space-x-1 content-center">
-									{#each links as link}
-										{#if link.href === segment}
-											<a href={link.href} class="navlink hover:bg-gray-200" aria-current="page">{link.text}</a>
-										{:else}
-											<a href={link.href} class="navlink hover:bg-gray-200">{link.text}</a>
-										{/if}
-									{/each}
+							{#if links.length}
+								<div class="hidden md:block">
+									<div class="ml-2 flex items-baseline space-x-1 content-center">
+										{#each links as link}
+											{#if link.href === segment}
+												<a href={link.href} class="navlink hover:bg-gray-200" aria-current="page">{link.text}</a>
+											{:else}
+												<a href={link.href} class="navlink hover:bg-gray-200">{link.text}</a>
+											{/if}
+										{/each}
+									</div>
 								</div>
-							</div>
-							<div class="ml-2 flex md:hidden">
-								<!-- Mobile menu button -->
-								<button on:click={nav.toggle} class="inline-flex items-center justify-center p-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-expanded={$nav || undefined}>
-									<span class="sr-only">Open main menu</span>
-									<!-- Icon when menu is closed. -->
-									<svg class:block={!$nav} class:hidden={$nav} class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-									</svg>
-									<!-- Icon when menu is open. -->
-									<svg class:block={$nav} class:hidden={!$nav} class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-									</svg>
-								</button>
-							</div>
+								<div class="ml-2 flex md:hidden">
+									<!-- Mobile menu button -->
+									<button on:click={nav.toggle} class="inline-flex items-center justify-center p-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-expanded={$nav || undefined}>
+										<span class="sr-only">Open main menu</span>
+										<!-- Icon when menu is closed. -->
+										<svg class:block={!$nav} class:hidden={$nav} class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+											<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+										</svg>
+										<!-- Icon when menu is open. -->
+										<svg class:block={$nav} class:hidden={!$nav} class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+											<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+										</svg>
+									</button>
+								</div>
+							{/if}
 						</div>
 						<div class="block">
 							<div class="ml-2 flex items-center md:ml-6">
@@ -108,22 +110,24 @@
 			</div>
 		</div>
 	</div>
-	<!--
-		Mobile menu, toggle classes based on menu state.
-		
-		Menu open: "block", Menu closed: "hidden"
-	-->
-	<div class:block={$nav} class:hidden={!$nav} class="md:hidden">
-		<div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-		{#each links as link}
-			{#if link.href === segment}
-				<a href={link.href} class="block navlink hover:bg-gray-200" aria-current="page">{link.text}</a>
-			{:else}
-				<a href={link.href} class="block navlink hover:bg-gray-200">{link.text}</a>
-			{/if}
-		{/each}
+	{#if links.length}
+		<!--
+			Mobile menu, toggle classes based on menu state.
+			
+			Menu open: "block", Menu closed: "hidden"
+		-->
+		<div class:block={$nav} class:hidden={!$nav} class="md:hidden">
+			<div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+			{#each links as link}
+				{#if link.href === segment}
+					<a href={link.href} class="block navlink hover:bg-gray-200" aria-current="page">{link.text}</a>
+				{:else}
+					<a href={link.href} class="block navlink hover:bg-gray-200">{link.text}</a>
+				{/if}
+			{/each}
+			</div>
 		</div>
-	</div>
+	{/if}
 </nav>
 
 <!-- Hidden links to redirect Client side rendering app  -->
