1f863d698fc662664c32903a4c7e5b62180f6b1a
victorjourne <journe.victor@gmail.com>
2021-02-19 19:41:30 +0100

    Change color and size of index texts.

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -5,7 +5,7 @@
 </svelte:head>
 <div class="background">
 
-<div class="preview text-justify">
+<div class="preview text-justify" use:cssProps={$userTheme.index}>
 <div>
 {#if ($user.resources.includes("description") && description.length > 0)}
 	{@html marked(description)}
@@ -81,6 +81,8 @@
 <script>
 import { onMount, onDestroy } from 'svelte';
 import { user, displayLogin } from '../components/stores.js';
+import { cssProps } from '../common/css-props.action';
+import { userTheme } from '../common/theme.store';
 import marked from 'marked'
 let description = "";
 let notice = "";
