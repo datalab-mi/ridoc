8e04c9f3623bed9e5301401db49b82ba3dc9644c
aurelienmart <martinez.aurelien@hotmail.fr>
2021-07-09 17:43:28 +0200

    ajout des liens dans env.json

--- frontend/src/layouts/Footer.svelte
+++ frontend/src/layouts/Footer.svelte
@@ -26,26 +26,26 @@
 	<div class=" mx-0 my-0 p-0 grid grid-cols-3 float-right">
 
 		<div class='text-left ml-10' >
-			<div class='font-bold text-blue-400 mb-4' >Nous connaître</div>
-			<a href={$envJson.plan} class='text-blue-800 underline'>Plan du site </a> <br>
-			<a href={$envJson.contact} class='text-blue-800  underline'>Nous contacter</a><br>
-			<a href={$envJson.horaires} class='text-blue-800 underline'>Horaires et plan d'accès</a><br>
+			<div class='font-bold text-blue-400 mb-4' style='color:#5770BE'>Nous connaître</div>
+			<a href={$envJson.plan} class='text-blue-800 underline'style='color:#000091'>Plan du site </a> <br>
+			<a href={$envJson.contact} class='text-blue-800  underline'style='color:#000091'>Nous contacter</a><br>
+			<a href={$envJson.horaires} class='text-blue-800 underline'style='color:#000091'>Horaires et plan d'accès</a><br>
 		</div>
 
 		<div class='text-left ml-10 ' >
-			<div class='font-bold text-blue-400 mb-4' >Nos engagements</div>
-			<a href={$envJson.engagement} class='text-blue-800 underline'>Engagement et qualité </a> <br>
-			<a href={$envJson.accessibilité} class='text-blue-800  underline'>Accessibilité</a><br>
-			<a href={$envJson.donnees} class='text-blue-800  underline'>Données personnelles et sécurité</a><br>
-			<a href={$envJson.mentions} class='text-blue-800 underline'>Mentions légales</a><br>
+			<div class='font-bold text-blue-400 mb-4' style='color:#5770BE'>Nos engagements</div>
+			<a href={$envJson.engagement} class='text-blue-800 underline'style='color:#000091'>Engagement et qualité </a> <br>
+			<a href={$envJson.accessibilite} class='text-blue-800  underline'style='color:#000091'>Accessibilité</a><br>
+			<a href={$envJson.donnees} class='text-blue-800  underline'style='color:#000091'>Données personnelles et sécurité</a><br>
+			<a href={$envJson.mentions} class='text-blue-800 underline'style='color:#000091'>Mentions légales</a><br>
 		</div>
 
 		<div class='text-left ml-10 ' >
-			<div class='font-bold  text-blue-400 mb-4' >Sites connexes</div>
-			<a href={$envJson.servicepublic} class='text-blue-800  underline'>service-public.fr </a> <br>
-			<a href={$envJson.datagouv} class='text-blue-800  underline'>data.gouv.fr</a><br>
-			<a href={$envJson.legifrance} class='text-blue-800 underline'>legifrance.gouv.fr</a><br>
-			<a href={$envJson.gouvernement} class='text-blue-800  underline'>gouvernement.fr</a><br>
+			<div class='font-bold  text-blue-400 mb-4' style='color:#5770BE'>Sites connexes</div>
+			<a href={$envJson.servicepublic} class='text-blue-800  underline'style='color:#000091'>service-public.fr </a> <br>
+			<a href={$envJson.datagouv} class='text-blue-800  underline'style='color:#000091'>data.gouv.fr</a><br>
+			<a href={$envJson.legifrance} class='text-blue-800 underline'style='color:#000091'>legifrance.gouv.fr</a><br>
+			<a href={$envJson.gouvernement} class='text-blue-800  underline'style='color:#000091'>gouvernement.fr</a><br>
 		</div>
 	</div>
 </footer>
