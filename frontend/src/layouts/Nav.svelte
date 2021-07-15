d05fe7748ad78f30ccd1b950ea7d78ac616f1be9
aurelienmart <martinez.aurelien@hotmail.fr>
2021-07-15 12:19:55 +0200

    ajout couleur config

--- frontend/src/layouts/Nav.svelte
+++ frontend/src/layouts/Nav.svelte
@@ -40,7 +40,7 @@
 	[aria-current]::after {
 		content: '';
 		height: 3px;
-		background-color: blue;
+		background-color:var(--primary);
 		@apply block;
 		@apply absolute;
 		@apply bottom-0;
@@ -58,6 +58,9 @@
 		@apply font-normal;
 	}
 
+	.connect{
+		background-color:var(--primary)
+	}
 	.artifact {
 	display: none;
 }
@@ -105,7 +108,7 @@
 						<div class="absolute right-0">
 							<div class="ml-2 ">
 								<!-- Profile dropdown -->
-								<div class="mr-4 bg-blue-700 p-2 rounded-lg">
+								<div class="connect mr-4  p-2 rounded-lg">
 									<div>
 										<button on:click={authClicked} class="text-white" color='white' id="user-menu" aria-haspopup="true">
 											<svg class="h-4 w-4 float-left m-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"/></svg>
