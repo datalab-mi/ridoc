cde695f4196f342c7385d9d2060f2fb03bab9ae2
aurelienmart <martinez.aurelien@hotmail.fr>
2021-08-03 15:02:35 +0200

    add searchRate

--- frontend/src/components/rate.svelte
+++ frontend/src/components/rate.svelte
@@ -1,15 +1,39 @@
 <script>
 import {displayComment} from "./stores"
 import {clickOutside} from "./click-outside.action"
+import {searchRateJson} from "./user-data.store"
+
+export let filename;
+let evaluate=false;
 function dislike() {
     $displayComment=!$displayComment
 }
 let comment
 const handleClickOutsideCom = (event) => {
 		$displayComment = false;
-        console.log("here")
 	};
 
+function addcomment(){
+    if (!evaluate){
+    $searchRateJson['bad'].push([filename.split('.')[0],comment])
+    $displayComment=false
+    console.log($searchRateJson) //add link to database
+    evaluate=true}
+    else{
+        window.alert("Merci pour votre aide, une seule réponse suffit")
+        $displayComment=false
+    }
+}
+function good(){
+    if (!evaluate){
+        $searchRateJson['good'].push(filename)//add link to database
+        evaluate=true
+    }
+    else{
+        window.alert("Merci pour votre aide, une seule réponse suffit")
+    }
+    
+}
     //inserer fonctions bon et mauvais resultats
 
 </script>
@@ -18,7 +42,7 @@ const handleClickOutsideCom = (event) => {
     <button class="dislike m-2 p-1" on:click={dislike}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
     </button>
-    <button class="like m-2 p-1">
+    <button class="like m-2 p-1" on:click={good}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
     </button>
 </div>
@@ -27,7 +51,7 @@ const handleClickOutsideCom = (event) => {
 <div class="titre text-xl"><b class="underline">Aidez nous</b> à nous améliorer</div>
 <p>Pouvez-vous décrire pourquoi ce résultat de vous convient pas ?</p>
 <textarea bind:value={comment} class="border-2 border-gray"></textarea>
-<button class="envoyer py-1 px-2 text-white " >Envoyer</button>
+<button class="envoyer py-1 px-2 text-white " on:click={addcomment} >Envoyer</button>
 </div>
 {/if}
 
