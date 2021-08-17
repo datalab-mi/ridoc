dd8d036f10acd5d39277f732baf1a04490730d0d
victorjourne <journe.victor@gmail.com>
2021-08-17 12:11:38 +0200

    Typo

--- frontend/src/components/emoRating/starrating.svelte
+++ frontend/src/components/emoRating/starrating.svelte
@@ -17,7 +17,7 @@
 
   <div class="Rateblock float-right mt-12 bg-white p-4 max-w-xs">
   <div class="titre text-xl"><b class="underline">Votre avis</b> nous intéresse</div>
-  <p class="break-normal">Comment décririez-vous votre expèrience sur le site ?</p>
+  <p class="break-normal">Comment décririez-vous votre expérience sur le site ?</p>
   <Rate
     {beforeRate}
     {afterRate}
