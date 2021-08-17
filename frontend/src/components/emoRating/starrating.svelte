6fa5e7a4e0c290909832e6962006e42009fb2dd3
victorjourne <journe.victor@gmail.com>
2021-08-17 12:05:04 +0200

    rm docker-compose-elasticsearch-huge.yml

--- frontend/src/components/emoRating/starrating.svelte
+++ frontend/src/components/emoRating/starrating.svelte
@@ -1,21 +1,20 @@
 <script>
     import Rate from "./Rate.svelte";
     import {rateJson} from "../user-data.store";
-  
+
     const beforeRate = rate => {
       console.log(rate);
     };
     const afterRate = rate => {
       if ($rateJson.length<1){
       $rateJson.push(rate)
-      console.log($rateJson) //insérer ici lien vers la BDD
-      }
-      else{
+      console.log($rateJson) // TODO: insérer ici lien vers la BDD
+      } else {
         window.alert("Vous avez déjà voté")
       }
     };
   </script>
-  
+
   <div class="Rateblock float-right mt-12 bg-white p-4 max-w-xs">
   <div class="titre text-xl"><b class="underline">Votre avis</b> nous intéresse</div>
   <p class="break-normal">Comment décririez-vous votre expèrience sur le site ?</p>
@@ -25,7 +24,7 @@
     length={5}
     ratedesc={['Très mauvaise', 'Mauvaise', 'Normale', 'Bonne', 'Très bonne']}
     showCount={true} />
-  
+
   </div>
 
   <style>
