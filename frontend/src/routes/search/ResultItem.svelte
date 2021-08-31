0ea8a6b0f833985023590b52118ca7ea6d62e387
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-08-31 15:09:26 +0200

    DSFR/resp header, Aside

--- frontend/src/routes/search/ResultItem.svelte
+++ frontend/src/routes/search/ResultItem.svelte
@@ -86,34 +86,34 @@
 
 		<div slot="buttons" class="pt-4">
 
-			<button on:click={http.fetchBlob(url)} {...btnAttrs} style={$envJson.primary}>
+			<button on:click={http.fetchBlob(url)} {...btnAttrs} style={$envJson.primary} class="fr-btn">
 				<svg {...svgAttrs}><path d="M.2 10a11 11 0 0 1 19.6 0A11 11 0 0 1 .2 10zm9.8 4a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>
 				<span>CONSULTER</span>
 			</button>
 
 			{#if $user.role === 'admin'}
-			<button on:click={handleDelete} {...btnAttrs} style={$envJson.primary}>
+			<button on:click={handleDelete} {...btnAttrs} style={$envJson.primary} class="fr-btn">
 				<svg {...svgAttrs}><path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/></svg>
 				<span>SUPPRIMER</span>
 			</button>
 			{#if canBeChange }
 					{#if readonly && send}
-						<button on:click={handleSave} {...btnAttrs}>
+						<button on:click={handleSave} {...btnAttrs} class="fr-btn">
 							<svg {...svgAttrs}><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
 							<span>MODIFIER</span>
 						</button>
 						<PutItem meta={meta} file={file} />
 					{:else if readonly || send}
-						<button on:click={() => readonly = !readonly} {...btnAttrs}>
+						<button on:click={() => readonly = !readonly} {...btnAttrs} class="fr-btn">
 							<svg {...svgAttrs}><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
 							<span>MODIFIER</span>
 						</button>
 					{:else}
-						<button on:click={handleSave} {...btnAttrs}>
+						<button on:click={handleSave} {...btnAttrs} class="fr-btn">
 							<svg {...svgAttrs}><path d="M0 2C0 .9.9 0 2 0h14l4 4v14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5 0v6h10V2H5zm6 1h3v4h-3V3z"/></svg>
 							<span>SAUVER</span>
 						</button>
-						<button on:click={handleCancel} {...btnAttrs}>
+						<button on:click={handleCancel} {...btnAttrs} class="fr-btn">
 							<svg {...svgAttrs}><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
 							<span>ANNULER</span>
 						</button>
@@ -133,25 +133,5 @@
 {/if}
 
 <style>
-	button {
-		@apply mt-1;
-		@apply px-4;
-		@apply py-2;
-		@apply text-white;
-		@apply font-bold;
-		@apply rounded;
-		@apply inline-flex;
-		@apply items-center;
-		@apply mx-0;
-		background-color: var(--primary);
 
-
-
-	}
-
-	button:hover {
-		@apply underline;
-		background-color: var(--primary);
-
-	}
 </style>
